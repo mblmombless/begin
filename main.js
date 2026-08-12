53086e69cecf906375219a6a9c16fcea27a29087
ulfthella <ulfthe.lla@gmail.com>
2026-08-12 12:03:58 +0800

    Update main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("btn");
+const text = document.getElementById("text");
+
+button.addEventListener("click", function () {
+    text.textContent = "Qosjowjw";
+    text.style.color = "blue";
+});
+
+console.log("html tolong diisi😇");
